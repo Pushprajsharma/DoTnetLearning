fef395060bcb8b38604476a86fe4bc4c3e221fb9
Pushprajsharma <73210153+Pushprajsharma@users.noreply.github.com>
2022-07-27 15:01:26 +0530

    Add files via upload

---
+++ Day3 Task1/Repository/Emp.cs
@@ -0,0 +1,46 @@
+﻿using WebApplication3.Models;
+namespace WebApplication3.Repository
+{
+    public class Emp : IEmployee
+    {
+        EmployeeDbcontext _context;
+
+        public Emp(EmployeeDbcontext context)
+        {
+            _context = context;
+        }
+
+        public void AddEmployee(Employee obj)
+        {
+            _context.employees.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+           Employee obj =  _context.employees.Find(id);
+           _context.employees.Remove(obj);
+            _context.SaveChanges();
+        }
+
+        public List<Employee> GetAllEMployee()
+        {
+            List<Employee> empList = _context.employees.ToList();
+            return empList;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            Employee obj = _context.employees.Find(id);
+            return obj;
+        }
+
+        public void UpdateEmployee(Employee obj)
+        {
+
+            _context.employees.Update(obj);
+            _context.SaveChanges();
+
+        }
+    }
+}
---
+++ Day3 Task1/Repository/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication3.Models;
+namespace WebApplication3.Repository
+{
+    public interface IEmployee
+    {
+        List<Employee> GetAllEMployee();
+        Employee GetEmployee(int id);
+
+        public void AddEmployee(Employee obj);
+        public void DeleteEmployee(int id);
+
+        public void UpdateEmployee(Employee obj);
+    }
+}
