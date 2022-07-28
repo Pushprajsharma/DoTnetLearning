46f9c285de4f71eef104685a0cdf64bb62d42619
Pushprajsharma <73210153+Pushprajsharma@users.noreply.github.com>
2022-07-28 14:09:31 +0530

    Add files via upload

---
+++ Day4 Task1/Controllers/EmployeeController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+using WebApplication3.Repository;
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        IEmployee _employee;
+        public EmployeeController(IEmployee employee)
+        {
+            _employee = employee;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AllEMployee()
+        {
+            List<Employee> employee = _employee.GetAllEMployee();
+            return View(employee);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Employee obj = _employee.GetEmployee(id);
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+;        }
+        [HttpPost]
+        public IActionResult Create(Employee obj)
+        {
+            _employee.AddEmployee(obj);
+            return RedirectToAction("AllEMployee");
+            
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) 
+        {
+            Employee employee = _employee.GetEmployee(id);
+            return View(employee);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+
+        public IActionResult DeleteEmployee(int id)
+        {
+            _employee.DeleteEmployee(id);
+            return RedirectToAction("AllEMployee");
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Edit(int id) {
+            Employee obj = _employee.GetEmployee(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee obj)
+        {
+            _employee.UpdateEmployee(obj);
+            return RedirectToAction("AllEMployee");
+        }
+        [HttpGet]
+        public IActionResult employeeByDeptNo(int id) 
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("employeeByDeptNo")]
+        public IActionResult getAllemployeeByDeptNo(int deptno)
+        {
+            IEnumerable<Employee> emplist = _employee.GetEmployeeByDeptNumber(deptno);
+            ViewBag.RequestType = Request.Method;
+            return View(emplist);
+        }
+
+        [HttpGet]
+        public IActionResult employeeByJob(string job)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("employeeByJob")]
+        public IActionResult getemployeeByJob(string job)
+        {
+            IEnumerable<Employee> emplist = _employee.GetEmployeeByJob(job);
+            ViewBag.RequestType = Request.Method;
+            return View(emplist);
+        }
+
+
+
+
+
+    }
+}
